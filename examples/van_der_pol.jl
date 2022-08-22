9c9fe0575e17d8d3d1f14188e8187ad228d967bd
Max Cohen <maxcohen@bu.edu>
2022-08-22 12:00:26 -0400

    Update image

--- examples/van_der_pol.jl
+++ examples/van_der_pol.jl
@@ -18,18 +18,21 @@ xs = -5.0:0.5:5.0
 ys = -5.0:0.5:5.0
 
 # Coordinates for initial conditions of phase portraits and length of corresponding sim
-xs_phase = -3.0:0.5:3.0
-ys_phase = -3.0:0.5:3.0
+xs_phase = -3.0:1.0:3.0
+ys_phase = -3.0:1.0:3.0
 T = 10.0
 
 # Plot the vector field
-fig = plot_vector_field(xs, ys, f, scale=0.35)
-plot_phase_portrait!(xs_phase, ys_phase, f, T)
-xlabel!(L"x")
-ylabel!(L"y")
-title!("Van der Pol oscillator")
-xlims!(-5, 5)
-display(fig)
+begin
+    fig = plot(dpi=200)
+    plot_vector_field!(xs, ys, f, scale=0.4)
+    plot_phase_portrait!(xs_phase, ys_phase, f, T, lw=2.5)
+    xlabel!(L"x")
+    ylabel!(L"y")
+    title!("Van der Pol oscillator")
+    xlims!(-5, 5)
+    display(fig)
+end
 
 # Save figure
 # savefig(fig, "van_der_pol.png")
